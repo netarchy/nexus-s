3bbe199ba6f80dfb4d3fa43e248bf99ce6d89cfd
Michael Huang <mike.g.huang@gmail.com>
2011-01-29 02:06:56 -0800

    ARM: S5PC11X: CPUFREQ: Fix power suspend cpu frequency
    Power suspend often crashes the device if the cpu frequency is not
    equal to SLEEP_FREQ. If the current cpufreq policy does not include
    SLEEP_FREQ, cpufreq_driver_target() does not change the cpu frequency
    for power suspend, resulting in crashes. This patch modifies the
    current cpufreq policy to include SLEEP_FREQ for suspend, and
    restores the cpufreq policy after suspend.
    
    Change-Id: I45ac6f959365870c3ef05e31a8befd52c1231f42
    Signed-off-by: Michael Huang <mike.g.huang@gmail.com>

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -704,19 +704,27 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
+	static int max, min;
 	int ret;
+
+	struct cpufreq_policy *policy = cpufreq_cpu_get(0);
 
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
-		ret = cpufreq_driver_target(cpufreq_cpu_get(0), SLEEP_FREQ,
+		max = policy->max;
+		min = policy->min;
+		policy->max = policy->min = SLEEP_FREQ;
+		ret = cpufreq_driver_target(policy, SLEEP_FREQ,
 				DISABLE_FURTHER_CPUFREQ);
 		if (ret < 0)
 			return NOTIFY_BAD;
 		return NOTIFY_OK;
 	case PM_POST_RESTORE:
 	case PM_POST_SUSPEND:
-		cpufreq_driver_target(cpufreq_cpu_get(0), SLEEP_FREQ,
+		cpufreq_driver_target(policy, SLEEP_FREQ,
 				ENABLE_FURTHER_CPUFREQ);
+		policy->max = max;
+		policy->min = min;
 		return NOTIFY_OK;
 	}
 	return NOTIFY_DONE;
