4304f2c13d1e6781dddfc893a34aa350fecb4222
netarchy <netarchy@gmail.com>
2011-02-25 19:37:11 -0600

    Extend new voltage tables for overclocking speeds

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -411,6 +411,21 @@ static struct s5p_media_device herring_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
+		.freq	= 1400000,
+		.varm	= 1450000,
+		.vint	= 1150000,
+	}, {
+	{
+		.freq	= 1300000,
+		.varm	= 1400000,
+		.vint	= 1150000,
+	}, {
+	{
+		.freq	= 1200000,
+		.varm	= 1350000,
+		.vint	= 1150000,
+	}, {
+	{
 		.freq	= 1000000,
 		.varm	= 1275000,
 		.vint	= 1100000,
