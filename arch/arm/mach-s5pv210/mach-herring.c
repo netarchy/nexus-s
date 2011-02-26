4cc178f48a1549d51a00a22e8030c6aa22e3c949
netarchy <netarchy@gmail.com>
2011-02-25 19:38:34 -0600

    Fix stupid typos

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -415,17 +415,14 @@ static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 		.varm	= 1450000,
 		.vint	= 1150000,
 	}, {
-	{
 		.freq	= 1300000,
 		.varm	= 1400000,
 		.vint	= 1150000,
 	}, {
-	{
 		.freq	= 1200000,
 		.varm	= 1350000,
 		.vint	= 1150000,
 	}, {
-	{
 		.freq	= 1000000,
 		.varm	= 1275000,
 		.vint	= 1100000,
