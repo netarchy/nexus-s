ee7a6deab4fe6f1d9fa9ddd2519479f114ecb86d
netarchy <netarchy@gmail.com>
2011-04-17 16:48:11 -0500

    Revert "sched: normalize sleeper's vruntime during group change"
    This reverts commit 9234f00a7b69d2096b7860a43bfd0b1962e19513.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1077,7 +1077,6 @@ struct sched_class {
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	void (*moved_group) (struct task_struct *p, int on_rq);
-	void (*prep_move_group) (struct task_struct *p, int on_rq);
 #endif
 };
 
