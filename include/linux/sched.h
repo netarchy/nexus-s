21d825b1a391d513fbd0bcb2441869a03f4aa14e
netarchy <netarchy@gmail.com>
2011-04-17 16:45:53 -0500

    Revert "BFS 363"
    This reverts commit b8f3577c58f7ae3e3748683bcadb3c992841e8cc.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -36,15 +36,8 @@
 #define SCHED_FIFO		1
 #define SCHED_RR		2
 #define SCHED_BATCH		3
-/* SCHED_ISO: Implemented on BFS only */
+/* SCHED_ISO: reserved but not implemented yet */
 #define SCHED_IDLE		5
-#ifdef CONFIG_SCHED_BFS
-#define SCHED_ISO		4
-#define SCHED_IDLEPRIO		SCHED_IDLE
-#define SCHED_MAX		(SCHED_IDLEPRIO)
-#define SCHED_RANGE(policy)	((policy) <= SCHED_MAX)
-#endif
-
 /* Can be ORed in to make sure the process is reverted back to SCHED_NORMAL on fork */
 #define SCHED_RESET_ON_FORK     0x40000000
 
@@ -274,6 +267,8 @@ extern void sched_init_smp(void);
 extern asmlinkage void schedule_tail(struct task_struct *prev);
 extern void init_idle(struct task_struct *idle, int cpu);
 extern void init_idle_bootup_task(struct task_struct *idle);
+
+extern int runqueue_is_locked(int cpu);
 
 extern cpumask_var_t nohz_cpu_mask;
 #if defined(CONFIG_SMP) && defined(CONFIG_NO_HZ)
@@ -1179,31 +1174,17 @@ struct task_struct {
 
 	int lock_depth;		/* BKL lock depth */
 
-#ifndef CONFIG_SCHED_BFS
 #ifdef CONFIG_SMP
 #ifdef __ARCH_WANT_UNLOCKED_CTXSW
 	int oncpu;
 #endif
 #endif
-#else /* CONFIG_SCHED_BFS */
-	int oncpu;
-#endif
 
 	int prio, static_prio, normal_prio;
 	unsigned int rt_priority;
-#ifdef CONFIG_SCHED_BFS
-	int time_slice;
-	u64 deadline;
-	struct list_head run_list;
-	u64 last_ran;
-	u64 sched_time; /* sched_clock time spent running */
-
-	unsigned long rt_timeout;
-#else /* CONFIG_SCHED_BFS */
 	const struct sched_class *sched_class;
 	struct sched_entity se;
 	struct sched_rt_entity rt;
-#endif
 
 #ifdef CONFIG_PREEMPT_NOTIFIERS
 	/* list of struct preempt_notifier: */
@@ -1298,9 +1279,6 @@ struct task_struct {
 	int __user *clear_child_tid;		/* CLONE_CHILD_CLEARTID */
 
 	cputime_t utime, stime, utimescaled, stimescaled;
-#ifdef CONFIG_SCHED_BFS
-	unsigned long utime_pc, stime_pc;
-#endif
 	cputime_t gtime;
 #ifndef CONFIG_VIRT_CPU_ACCOUNTING
 	cputime_t prev_utime, prev_stime;
@@ -1523,67 +1501,6 @@ struct task_struct {
 #endif
 };
 
-#ifdef CONFIG_SCHED_BFS
-extern int grunqueue_is_locked(void);
-extern void grq_unlock_wait(void);
-#define tsk_seruntime(t)		((t)->sched_time)
-#define tsk_rttimeout(t)		((t)->rt_timeout)
-
-static inline void set_oom_timeslice(struct task_struct *p)
-{
-	p->time_slice = HZ;
-}
-
-static inline void tsk_cpus_current(struct task_struct *p)
-{
-}
-
-#define runqueue_is_locked(cpu)	grunqueue_is_locked()
-
-static inline void print_scheduler_version(void)
-{
-	printk(KERN_INFO"BFS CPU scheduler v0.363 by Con Kolivas.\n");
-}
-
-static inline int iso_task(struct task_struct *p)
-{
-	return (p->policy == SCHED_ISO);
-}
-extern void remove_cpu(unsigned long cpu);
-#else /* CFS */
-extern int runqueue_is_locked(int cpu);
-#define tsk_seruntime(t)	((t)->se.sum_exec_runtime)
-#define tsk_rttimeout(t)	((t)->rt.timeout)
-
-static inline void sched_exit(struct task_struct *p)
-{
-}
-
-static inline void set_oom_timeslice(struct task_struct *p)
-{
-	p->rt.time_slice = HZ;
-}
-
-static inline void tsk_cpus_current(struct task_struct *p)
-{
-	p->rt.nr_cpus_allowed = current->rt.nr_cpus_allowed;
-}
-
-static inline void print_scheduler_version(void)
-{
-	printk(KERN_INFO"CFS CPU scheduler.\n");
-}
-
-static inline int iso_task(struct task_struct *p)
-{
-	return 0;
-}
-
-static inline void remove_cpu(unsigned long cpu)
-{
-}
-#endif /* CONFIG_SCHED_BFS */
-
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
 #define tsk_cpus_allowed(tsk) (&(tsk)->cpus_allowed)
 
@@ -1602,19 +1519,9 @@ static inline void remove_cpu(unsigned long cpu)
 
 #define MAX_USER_RT_PRIO	100
 #define MAX_RT_PRIO		MAX_USER_RT_PRIO
-#define DEFAULT_PRIO		(MAX_RT_PRIO + 20)
 
-#ifdef CONFIG_SCHED_BFS
-#define PRIO_RANGE		(40)
-#define MAX_PRIO		(MAX_RT_PRIO + PRIO_RANGE)
-#define ISO_PRIO		(MAX_RT_PRIO)
-#define NORMAL_PRIO		(MAX_RT_PRIO + 1)
-#define IDLE_PRIO		(MAX_RT_PRIO + 2)
-#define PRIO_LIMIT		((IDLE_PRIO) + 1)
-#else /* CONFIG_SCHED_BFS */
 #define MAX_PRIO		(MAX_RT_PRIO + 40)
-#define NORMAL_PRIO		DEFAULT_PRIO
-#endif /* CONFIG_SCHED_BFS */
+#define DEFAULT_PRIO		(MAX_RT_PRIO + 20)
 
 static inline int rt_prio(int prio)
 {
@@ -1926,7 +1833,7 @@ task_sched_runtime(struct task_struct *task);
 extern unsigned long long thread_group_sched_runtime(struct task_struct *task);
 
 /* sched_exec is called by processes performing an exec */
-#if defined(CONFIG_SMP) && !defined(CONFIG_SCHED_BFS)
+#ifdef CONFIG_SMP
 extern void sched_exec(void);
 #else
 #define sched_exec()   {}
@@ -2090,9 +1997,6 @@ extern void wake_up_new_task(struct task_struct *tsk,
  static inline void kick_process(struct task_struct *tsk) { }
 #endif
 extern void sched_fork(struct task_struct *p, int clone_flags);
-#ifdef CONFIG_SCHED_BFS
-extern void sched_exit(struct task_struct *p);
-#endif
 extern void sched_dead(struct task_struct *p);
 
 extern void proc_caches_init(void);
--- init/main.c
+++ init/main.c
@@ -834,7 +834,6 @@ static noinline int init_post(void)
 	system_state = SYSTEM_RUNNING;
 	numa_default_policy();
 
-	print_scheduler_version();
 
 	current->signal->flags |= SIGNAL_UNKILLABLE;
 
