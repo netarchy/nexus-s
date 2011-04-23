7736348271e448ef7500f6aed6adfcaa5cbfe6c7
netarchy <netarchy@gmail.com>
2011-04-23 00:11:54 -0500

    raise arm voltage cap for adventurous over-volting individuals

--- arch/arm/mach-s5pv210/cpu-freq.c
+++ arch/arm/mach-s5pv210/cpu-freq.c
@@ -72,7 +72,7 @@ struct s5pv210_dvs_conf {
 	unsigned long       int_volt;   /* uV */
 };
 
-const unsigned long arm_volt_max = 1450000;
+const unsigned long arm_volt_max = 1500000;
 const unsigned long int_volt_max = 1250000;
 
 static struct s5pv210_dvs_conf dvs_conf[] = {
