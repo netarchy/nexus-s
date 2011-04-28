cfb00419898f866c5c629c79e99edcbcc4aa1ad6
Adam Kent <adam@semicircular.net>
2011-04-28 22:31:40 +1000

    i9023 BLN: Properly set sleep mode for LED pins
    Without this change, only the "menu" light was lighting up
    after the phone goes to full sleep mode.

--- arch/arm/mach-s5pv210/herring-touchkey-led.c
+++ arch/arm/mach-s5pv210/herring-touchkey-led.c
@@ -219,18 +219,21 @@ static int __init herring_init_touchkey_led(void)
 {
 	int i;
 	int ret = 0;
+	u32 gpio;
 
 	if (!machine_is_herring() || system_rev < 0x10)
 		return 0;
 
 	for (i = 0; i < ARRAY_SIZE(led_gpios); i++) {
-		ret = gpio_request(S5PV210_GPJ3(led_gpios[i]), "touchkey led");
+		gpio = S5PV210_GPJ3(led_gpios[i]);
+		ret = gpio_request(gpio, "touchkey led");
 		if (ret) {
 			pr_err("Failed to request touchkey led gpio %d\n", i);
 			goto err_req;
 		}
-		s3c_gpio_setpull(S5PV210_GPJ3(led_gpios[i]),
-							S3C_GPIO_PULL_NONE);
+		s3c_gpio_setpull(gpio, S3C_GPIO_PULL_NONE);
+		s3c_gpio_slp_cfgpin(gpio, S3C_GPIO_SLP_PREV);
+		s3c_gpio_slp_setpull_updown(gpio, S3C_GPIO_PULL_NONE);
 	}
 
 	herring_touchkey_led_onoff(1);
