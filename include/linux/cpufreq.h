db03482e9d24fb673f7e96930ab2abf91bffcdc2
Dylan <dylan.taber@gmail.com>
2011-05-07 13:31:14 -0500

    Add TheEscapists modified smartass governor, and fix the scaling for the 400mhz-800mhz gap

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
+extern struct cpufreq_governor cpufreq_gov_smartass;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass)
 #endif
 
 
