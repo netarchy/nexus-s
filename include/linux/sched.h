286e1539bafb2eb2594eacc285e1c0d74ddb04be
Con Kolivas <kernel@kolivas.org>
2011-05-17 19:05:27 -0500

    BFS 404

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1546,7 +1546,7 @@ static inline void tsk_cpus_current(struct task_struct *p)
 
 static inline void print_scheduler_version(void)
 {
-	printk(KERN_INFO"BFS CPU scheduler v0.401 by Con Kolivas.\n");
+	printk(KERN_INFO"BFS CPU scheduler v0.404 by Con Kolivas.\n");
 }
 
 static inline int iso_task(struct task_struct *p)
